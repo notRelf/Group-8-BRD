0ae4227e04eac588bab55a03e8484e46859a3e8f
Metiuwnie <adonaismatthew@gmail.com>
2025-09-26 19:41:26 +0700

    Add files via upload

---
+++ login/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+  <title>FIC Bruder Login</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="glass-container">
+    <img src="FIClogo.png" class="logo">
+    <form>
+      <div class="input-group">
+        <input type="text" id="username" name="username" placeholder="Username" required>
+      </div>
+      <div class="input-group">
+        <input type="password" id="password" name="password" placeholder="Password" required>
+      </div>
+      <button type="submit" class="login-button">Login</button>
+      <div class="admin-link">
+        <span><p><a href="admin_login.php">Login sebagai Admin </a></p></span>
+    </form>
+  </div>
+</body>
+</html>
